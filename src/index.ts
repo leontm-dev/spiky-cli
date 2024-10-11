cb239413ca2100512aadcfdebeec87763d9a701a
LeonTM <lanneken09@gmail.com>
2024-10-11 15:34:48 +0200

    The code I am starting with right now

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
 #!/usr/bin/env node
+
+// Imports
+
+import args from 'args';
+
+// Project-Imports
+
+import versionCommand from './commands/version.js';
+import helpCommand from './commands/help.js';
+
+// Code
+
+// Commands
+args.command('version', 'Prints the current version', versionCommand, ['v'])
+	.command('help', 'Displays help information', helpCommand)
+	// Options
+	.option(
+		['H', 'help'],
+		'Outputs usage info about the correct and successful usage of the spiky-cli',
+		undefined,
+		helpCommand
+	)
+	// Examples
+	.example('The current version is: vX.X.X', 'spiky version');
+
+const flags = args.parse(process.argv, {
+	help: false,
+	version: false,
+	name: 'spiky',
+	mainColor: 'yellowBright',
+	subColor: 'grey',
+	mri: {}
+});
