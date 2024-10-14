60106df8ce22b7d2ba28adfb5e53b41223ed653b
LeonTM <lanneken09@gmail.com>
2024-10-14 14:12:34 +0200

    Added some new commands

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 
 // Imports
 
-import args from 'args';
+import { program } from 'commander';
 
 // Project-Imports
 
@@ -10,29 +10,46 @@ import versionCommand from './commands/version.js';
 import helpCommand from './commands/help.js';
 import buildCommand from './commands/build.js';
 import initCommand from './commands/init.js';
+import updateCommand from './commands/update.js';
 
 // Code
 
 // Commands
-args.command('version', 'Prints the current version', versionCommand, ['v'])
-	.command('help', 'Displays help information', helpCommand)
-	.command('build', 'Builds the project', buildCommand, ['b'])
-	.command('init', 'Initializes the project', initCommand)
-	// Options
-	.option(
-		['H', 'help'],
-		'Outputs usage info about the correct and successful usage of the spiky-cli',
-		undefined,
-		helpCommand
-	)
-	// Examples
-	.example('The current version is: vX.X.X', 'spiky version');
 
-const flags = args.parse(process.argv, {
-	help: false,
-	version: false,
-	name: 'spiky',
-	mainColor: 'yellowBright',
-	subColor: 'grey',
-	mri: {}
+program.name('spiky');
+program.addCommand(
+	program
+		.createCommand('version')
+		.description('Prints the current version')
+		.action(versionCommand)
+		.alias('v'),
+	{ isDefault: false }
+);
+program.addCommand(
+	program
+		.createCommand('update')
+		.description('Updates the spiky-cli to the lastest version')
+		.action(updateCommand),
+	{ isDefault: false }
+);
+program.addCommand(
+	program
+		.createCommand('help')
+		.action(helpCommand)
+		.description('Displays help information'),
+	{
+		isDefault: true
+	}
+);
+program.addCommand(program.createCommand('build').action(buildCommand), {
+	isDefault: false
 });
+program.addCommand(
+	program
+		.createCommand('init')
+		.action((str, options) => initCommand(str, options))
+		.option('-y', 'Skips the prompt and uses the default values'),
+	{ isDefault: false }
+);
+
+program.parse(process.argv);
