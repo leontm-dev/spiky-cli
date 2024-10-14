6bf2411eced5908fb57e415516f5fef2643d7981
LeonTM <lanneken09@gmail.com>
2024-10-14 14:13:24 +0200

    Finished help command

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,11 +1,14 @@
 // Imports
 
-import args from 'args';
+import { program } from 'commander';
 
 // Project-Imports
 
+import checkForOutdatedVersion from '../functions/checkForOutdatedVersion.js';
+
 // Code
 
-export default function helpCommand() {
-	args.showHelp();
+export default async function helpCommand() {
+	await checkForOutdatedVersion();
+	program.help();
 }
