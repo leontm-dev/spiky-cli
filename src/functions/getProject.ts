2cc082f52e06139f662d87c88ec6fe36c938e599
LeonTM <lanneken09@gmail.com>
2024-10-14 18:54:38 +0200

    Added a new function to get the project data

---
+++ src/functions/getProject.ts
@@ -0,0 +1,37 @@
+// Imports
+
+import fs from 'fs';
+
+// Project-Imports
+
+import checkForCorrectCodebase from './checkForCorrectCodebase.js';
+
+// Code
+
+export default function getProject(folderName: string) {
+	if (!fs.existsSync(folderName)) {
+		return null;
+	}
+
+	if (!checkForCorrectCodebase(folderName)) {
+		return null;
+	}
+
+	const output = { manifest: {}, projectbody: {}, icon: '' };
+
+	const files = fs.readdirSync(folderName);
+	for (const file of files) {
+		if (file === 'manifest.json') {
+			output.manifest = JSON.parse(
+				fs.readFileSync(`${folderName}/manifest.json`, 'utf-8')
+			);
+		} else if (file === 'projectbody.json') {
+			output.projectbody = JSON.parse(
+				fs.readFileSync(`${folderName}/projectbody.json`, 'utf-8')
+			);
+		} else if (file === 'icon.png') {
+			output.icon = fs.readFileSync(`${folderName}/icon.png`, 'utf-8');
+		}
+	}
+	return output;
+}
