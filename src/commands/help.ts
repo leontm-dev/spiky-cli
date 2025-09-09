9d57c26481fe88d9c91e3e2d09f842bb57cb4e4b
Leon <LAnneken09@gmail.com>
2025-09-09 21:58:10 +0200

    Removed the version check from the help command

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -2,13 +2,8 @@
 
 import { program } from 'commander';
 
-// Project-Imports
-
-import checkForOutdatedVersion from '../functions/checkForOutdatedVersion.js';
-
 // Code
 
 export default async function helpCommand() {
-	await checkForOutdatedVersion();
 	program.help();
 }
