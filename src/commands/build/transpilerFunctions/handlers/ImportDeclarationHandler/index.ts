865af916b481169925433fd88b2a573f9d1edcc7
Leon <LAnneken09@gmail.com>
2025-09-11 22:29:20 +0200

    Updated the import declaration handler by implementing the default code handling

--- src/commands/build/transpilerFunctions/handlers/ImportDeclarationHandler/index.ts
+++ src/commands/build/transpilerFunctions/handlers/ImportDeclarationHandler/index.ts
@@ -2,48 +2,68 @@
 
 // Project-Imports
 
-import { NodePath } from '@babel/traverse';
-import { PythonImports } from './ImportsRecord.js';
-import { ImportDeclaration } from '@babel/types';
+import { NodePath } from "@babel/traverse";
+import { PythonImports } from "./ImportsRecord.js";
+import { ImportDeclaration } from "@babel/types";
+import {
+  Contents,
+  formatLeadingComments,
+  SubHandlerResponse,
+} from "../../transpileCode.js";
+import { calculateIndent } from "../../../../../lib/calculateIndent.js";
 
 // Code
 
 const onlyLeaveNotMatching = (str1: string, str2: string): string => {
-	const splittedStr1 = str1.split(' ');
-	const splittedStr2 = str2.split(' ');
+  const splittedStr1 = str1.split(" ");
+  const splittedStr2 = str2.split(" ");
 
-	const left: string[] = [];
+  const left: string[] = [];
 
-	splittedStr1.forEach(part => {
-		if (!splittedStr2.includes(part)) left.push(part);
-	});
+  splittedStr1.forEach((part) => {
+    if (!splittedStr2.includes(part)) left.push(part);
+  });
 
-	return left.join(' ');
+  return left.join(" ");
 };
 export default function ImportDeclarationHandler(
-	existingImports: string[],
-	path: NodePath<ImportDeclaration>
-): string[] {
-	const copyOfExistingImports = existingImports;
-	const imp = PythonImports[path.node.source.value];
-	if (!imp) return copyOfExistingImports;
+  code: Contents,
+  path: NodePath<ImportDeclaration>
+): SubHandlerResponse {
+  const contents: Contents = code;
+  const imp = PythonImports[path.node.source.value];
+  if (!imp) return { ok: false };
+  const indent = calculateIndent(path);
+  contents.push(...formatLeadingComments(path.node.leadingComments, indent));
 
-	if (imp.includes('from ') && imp.includes(' import ')) {
-		const fromWhat = imp.split(' import ')[0]!;
+  if (imp.includes("from ") && imp.includes(" import ")) {
+    const fromWhat = imp.split(" import ")[0]!;
 
-		const existingImportIndex = copyOfExistingImports.findIndex(i =>
-			i.includes(fromWhat)
-		);
-		if (existingImportIndex > -1) {
-			const item = copyOfExistingImports[existingImportIndex]!;
-			copyOfExistingImports.splice(existingImportIndex);
-			copyOfExistingImports.push(item + ', ' + onlyLeaveNotMatching(imp, item));
-		} else {
-			copyOfExistingImports.push(imp);
-		}
-	} else {
-		copyOfExistingImports.push(imp);
-	}
+    const existingImportIndex = contents.findIndex(
+      (i) => i.content.includes(fromWhat) && i.type === "import"
+    );
+    if (existingImportIndex > -1) {
+      const item = contents[existingImportIndex]!;
+      contents.splice(existingImportIndex);
+      contents.push({
+        content: item.content + ", " + onlyLeaveNotMatching(imp, item.content),
+        type: "import",
+        line: item.line,
+      });
+    } else {
+      contents.push({
+        content: imp,
+        type: "import",
+        line: path.node.loc?.start.line,
+      });
+    }
+  } else {
+    contents.push({
+      content: imp,
+      type: "import",
+      line: path.node.loc?.start.line,
+    });
+  }
 
-	return copyOfExistingImports;
+  return { ok: true, contents };
 }
