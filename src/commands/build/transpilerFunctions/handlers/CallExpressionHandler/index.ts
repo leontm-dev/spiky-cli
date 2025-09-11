0014c18bbe6a37a0e85d48fd7117b8a9d4b86797
Leon <LAnneken09@gmail.com>
2025-09-11 22:30:09 +0200

    Made changes according to previous ones

--- src/commands/build/transpilerFunctions/handlers/CallExpressionHandler/index.ts
+++ src/commands/build/transpilerFunctions/handlers/CallExpressionHandler/index.ts
@@ -1,33 +1,31 @@
 // Imports
 
-import { NodePath } from '@babel/traverse';
-import { CallExpression } from '@babel/types';
-import { RunloopHandler } from './BuildIns/Runloop.js';
-import { MotorPairHandler } from './BuildIns/MotorPair.js';
+import { NodePath } from "@babel/traverse";
+import { CallExpression } from "@babel/types";
+import { RunloopHandler } from "./BuildIns/Runloop.js";
+import { MotorPairHandler } from "./BuildIns/MotorPair.js";
+import { Contents, SubHandlerResponse } from "../../transpileCode.js";
 
 // Code
 
-export type CallExpressionHandlerReturn =
-	| { ok: true; c: { content: string; line?: number | undefined } }
-	| { ok: false };
 export default function CallExpressionHandler(
-	variables: {}[],
-	path: NodePath<CallExpression>
-): CallExpressionHandlerReturn {
-	const callee = path.node.callee;
-	if (
-		callee.type === 'MemberExpression' &&
-		callee.object.type === 'Identifier'
-	) {
-		switch (callee.object.name) {
-			case 'Runloop':
-				return RunloopHandler(path);
-			case 'MotorPair':
-				return MotorPairHandler(path);
-			default:
-				return { ok: false };
-		}
-	} else {
-		return { ok: false };
-	}
+  code: Contents,
+  path: NodePath<CallExpression>
+): SubHandlerResponse {
+  const callee = path.node.callee;
+  if (
+    callee.type === "MemberExpression" &&
+    callee.object.type === "Identifier"
+  ) {
+    switch (callee.object.name) {
+      case "Runloop":
+        return RunloopHandler(code, path);
+      case "MotorPair":
+        return MotorPairHandler(code, path);
+      default:
+        return { ok: false };
+    }
+  } else {
+    return { ok: false };
+  }
 }
