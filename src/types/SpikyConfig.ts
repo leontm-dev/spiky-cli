d17851749c4813853581a80eefb4e49ae82edf70
Leon <LAnneken09@gmail.com>
2025-09-11 17:20:53 +0200

    Added formatterSettings to the SpikyConfig file

--- src/types/SpikyConfig.ts
+++ src/types/SpikyConfig.ts
@@ -1,16 +1,26 @@
+// Imports
+
+import { Config } from "@wasm-fmt/ruff_fmt";
+
 // Code
 
 export type SpikyConfig = {
-	inputFileName: string;
-	existingProjectFolderName: string;
-	export:
-		| {
-				type: 'project';
-				folderName: string;
-		  }
-		| {
-				type: 'python';
-				exportFileName: string;
-				formatOutput: boolean;
-		  };
+  inputFileName: string;
+  existingProjectFolderName: string;
+  export:
+    | {
+        type: "project";
+        folderName: string;
+      }
+    | {
+        type: "python";
+        exportFileName: string;
+        formatOutput: false;
+      }
+    | {
+        type: "python";
+        exportFileName: string;
+        formatOutput: true;
+        formatterSettings: Config;
+      };
 };
