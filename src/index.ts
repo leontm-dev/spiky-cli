7b26896aca6f92219e878e58555b683a6ce8a8f7
Leon <LAnneken09@gmail.com>
2025-09-12 19:52:32 +0200

    Added description

--- src/index.ts
+++ src/index.ts
@@ -2,68 +2,69 @@
 
 // Imports
 
-import { program } from 'commander';
+import { program } from "commander";
 
 // Project-Imports
 
-import versionCommand from './commands/version.js';
-import helpCommand from './commands/help.js';
-import buildCommand from './commands/build/index.js';
-import initCommand from './commands/init/index.js';
-import updateCommand from './commands/update.js';
-import { SpikyConfig } from './types/SpikyConfig.js';
+import versionCommand from "./commands/version.js";
+import helpCommand from "./commands/help.js";
+import buildCommand from "./commands/build/index.js";
+import initCommand from "./commands/init/index.js";
+import updateCommand from "./commands/update.js";
+import { SpikyConfig } from "./types/SpikyConfig.js";
 
 // Code
 
 // Commands
 
-program.name('spiky');
+program.name("spiky");
 program.description(
-	'A cli tool that transpiles javascript code into python code.'
+  "A cli tool that transpiles javascript code into python code."
 );
 program.addCommand(
-	program
-		.createCommand('version')
-		.description('Prints the current version')
-		.action(versionCommand)
-		.alias('v'),
-	{ isDefault: false }
+  program
+    .createCommand("version")
+    .description("Prints the current version")
+    .action(versionCommand)
+    .alias("v"),
+  { isDefault: false }
 );
 program.addCommand(
-	program
-		.createCommand('update')
-		.alias('u')
-		.description('Updates the spiky-cli to the lastest version')
-		.action(updateCommand),
-	{ isDefault: false }
+  program
+    .createCommand("update")
+    .alias("u")
+    .description("Updates the spiky-cli to the lastest version")
+    .action(updateCommand),
+  { isDefault: false }
 );
 program.addCommand(
-	program
-		.createCommand('help')
-		.action(helpCommand)
-		.alias('a')
-		.description('Displays help information'),
-	{ isDefault: true }
+  program
+    .createCommand("help")
+    .action(helpCommand)
+    .alias("a")
+    .description("Displays help information"),
+  { isDefault: true }
 );
 program.addCommand(
-	program
-		.createCommand('build')
-		.description('Transpile your TypeScript Code into python.')
-		.alias('b')
-		.option('--overwrite', 'Instant overwriting?')
-		.option('--keeplines', 'Want to keep your lines?')
-		.action(buildCommand),
-	{
-		isDefault: false
-	}
+  program
+    .createCommand("build")
+    .description("Transpile your TypeScript Code into python.")
+    .alias("b")
+    .option("--overwrite", "Instant overwriting?")
+    .option("--keeplines", "Want to keep your lines?")
+    .action(buildCommand),
+  {
+    isDefault: false,
+  }
 );
 program.addCommand(
-	program
-		.createCommand('init')
-		.alias('i')
-		.action((str, options) => initCommand(str, options))
-		.option('-y', 'Skips the prompt and uses the default values'),
-	{ isDefault: false }
+  program
+    .createCommand("init")
+    .alias("i")
+    .action((str, options) => initCommand(str, options))
+    .description("Initialize a new config file.")
+    .option("-y", "Skips the prompt and uses the default values"),
+  { isDefault: false }
 );
 
 program.parse(process.argv);
