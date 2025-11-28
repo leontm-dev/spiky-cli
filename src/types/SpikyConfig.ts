6b4cf62151e3e993f65e4c3635e4daa2081e53ad
Leon <LAnneken09@gmail.com>
2025-11-28 14:18:47 +0100

    Updated the default type

--- src/types/SpikyConfig.ts
+++ src/types/SpikyConfig.ts
@@ -24,17 +24,17 @@ export type SpikyConfig = {
       };
 };
 export const SpikyConfigDefault: SpikyConfig = {
-	inputFileName: "input.spiky.ts",
-	export: { 
-		type: "python",
-		exportFileName: "export.spiky.py",
-		formatOutput: true,
-		formatterSettings: {
-			"indent_style": "tab",
-			"indent_width": 4,
-			"line_ending": "crlf",
-			"magic_trailing_comma": "respect",
-			"quote_style": "single"
-		}
-	}
-}
+  inputFileName: "input.spiky.ts",
+  export: {
+    type: "python",
+    exportFileName: "export.spiky.py",
+    formatOutput: true,
+    formatterSettings: {
+      indent_style: "tab",
+      indent_width: 4,
+      line_ending: "lf",
+      magic_trailing_comma: "respect",
+      quote_style: "single",
+    },
+  },
+};
