811e17aa48edd546995549ae9a213c9377d9ea0e
Leon <LAnneken09@gmail.com>
2025-11-28 17:09:43 +0100

    Removed babel types, functions and classes

--- src/commands/build/transpilerFunctions/transpileCode.ts
+++ src/commands/build/transpilerFunctions/transpileCode.ts
@@ -1,135 +1,70 @@
 // Imports
 
-import traverse from "@babel/traverse";
-import { parse } from "@babel/parser";
-import { Comment } from "@babel/types";
+import chalk from "chalk";
+import Transpiler from "ast-transpiler";
 
 // Project-Imports
 
 import { FunctionIndexes } from "../../../types/functionIndex.js";
 import formSteps from "../../../functions/formSteps.js";
 import updateOldConsole from "../../../functions/updateOldConsole.js";
-import chalk from "chalk";
-import ImportDeclarationHandler from "./handlers/ImportDeclarationHandler/index.js";
-import CallExpressionHandler from "./handlers/CallExpressionHandler/index.js";
-import FunctionDeclarationHandler from "./handlers/FunctionDeclarationHandler/index.js";
+import {
+  ITranspiledFile,
+  TranspilerConfig,
+} from "../../../types/transpiler.types.js";
 
 // Code
-
-export type SubHandlerResponse =
-  | {
-      ok: true;
-      contents: Contents;
-    }
-  | { ok: false };
-export type Content = {
-  content: string;
-  line?: number | undefined;
-  type: ContentType;
-};
-export type Contents = Content[];
-export type ContentType = "import" | "code" | "comment";
-
-export function formatLeadingComments(
-  comments: Comment[] | null | undefined,
-  indent: string
-): Contents {
-  if (!comments || comments.length === 0) {
-    return [];
-  }
-  return comments.map((comment) => ({
-    type: "comment",
-    content:
-      comment.type === "CommentBlock"
-        ? `${indent}/* ${comment.value} */`
-        : `${indent}# ${comment.value}`,
-    line: comment.loc?.start.line,
-  }));
-}
 
 export default function transpileCode(
   code: string,
   functionIndexes: FunctionIndexes
-):
-  | {
-      ok: true;
-      code: { content: string; line?: number | undefined }[];
-    }
-  | { ok: false } {
+): ITranspiledFile {
   process.stdout.write(
     `${chalk.yellowBright.italic(formSteps(functionIndexes))} Transpiling...`
   );
   try {
-    const ast = parse(code, {
-      sourceType: "module",
-      plugins: ["typescript"],
-      attachComment: true,
-    });
-    let transpiledCode: Contents = [];
-
-    traverse.default(ast, {
-      ImportDeclaration: function (path) {
-        const importsDeclarationHandlerResponse = ImportDeclarationHandler(
-          transpiledCode,
-          path
-        );
-        if (importsDeclarationHandlerResponse.ok) {
-          transpiledCode = importsDeclarationHandlerResponse.contents;
-        }
+    const transpilerConfig: TranspilerConfig = {
+      verbose: false,
+      python: {
+        FullPropertyAccessReplacements: {
+          "Runloop.run": "runloop.run",
+        },
+        LeftPropertyAccessReplacements: {
+          MotorPair: "motor_pair",
+          Runloop: "runloop",
+          Motor: "motor",
+        },
+        RightPropertyAccessReplacements: {
+          MotorPair: "motor_pair",
+          Runloop: "runloop",
+          Motor: "motor",
+        },
+        CallExpressionReplacements: {
+          MotorPair: "motor_pair",
+        },
       },
-      FunctionDeclaration: function (path) {
-        const functionDeclarationHandlerResponse = FunctionDeclarationHandler(
-          transpiledCode,
-          path
-        );
-        if (functionDeclarationHandlerResponse.ok) {
-          transpiledCode = functionDeclarationHandlerResponse.contents;
-        }
-      },
-      // ReturnStatement: function (path) {
-      // 	console.log('RETURN found', path.node.start);
-      // },
-      // ExpressionStatement: function (path) {
-      // 	console.log(path.data);
-      // },
-      CallExpression: function (path) {
-        const callExpressionHandlerResponse = CallExpressionHandler(
-          transpiledCode,
-          path
-        );
-        if (callExpressionHandlerResponse.ok) {
-          transpiledCode = callExpressionHandlerResponse.contents;
-        }
-      },
-      // MemberExpression: function (path) {
-      // 	if (path.node.object.type === 'Identifier') {
-      // 		switch (path.node.object.name) {
-      // 			case 'Runloop':
-      // 				if (path.node.property.type === 'Identifier') {
-      // 					if (path.node.property.name === 'run') {
-      // 						transpiledCode.push({
-      // 							content: 'runloop.run(',
-      // 							line: 1
-      // 						});
-      // 					}
-      // 				}
-      // 		}
-      // 	}
-      // }
-      // ArrowFunctionExpression: function (path) {
-      // 	console.log('FUNCTION Arrow');
-      // }
-    });
+    };
+    const transpiler = new Transpiler(transpilerConfig);
+    const content = transpiler.transpilePython(code) as ITranspiledFile;
     updateOldConsole(
-      `${chalk.greenBright.italic(formSteps(functionIndexes))} Transpiled code completely.`,
+      `${chalk.greenBright.italic(
+        formSteps(functionIndexes)
+      )} Transpiled code completely.`,
       true
     );
-    console.log(transpiledCode.map((c) => c.line));
-    return { ok: true, code: transpiledCode };
+    return {
+      ...content,
+      content: content.content
+        .replaceAll("MotorPair", "motor_pair")
+        .replaceAll("Runloop", "runloop")
+        .replaceAll("ColorSensor", "color_sensor"),
+    };
   } catch (error) {
     updateOldConsole(
-      `${chalk.redBright.italic(formSteps(functionIndexes))} Something unexpected happenend during transpilation.`
+      `${chalk.redBright.italic(
+        formSteps(functionIndexes)
+      )} Something unexpected happenend during transpilation.`
     );
-    return { ok: false };
+    throw new Error();
   }
 }
